c4b76af1d41d5761183a1c8e99800e87529681ef
= <NeNtoMid0@gmail.com>
2021-01-12 22:03:53 +0100

    Done setting up whole UI for calculator App

--- src/components/Calculations/Calculations.js
+++ src/components/Calculations/Calculations.js
@@ -8,7 +8,44 @@ import Block from './../UI/Block/Block';
 
 const Calculations = (props) => {
 	return (
-		<Grid container>
+		<Grid
+			container
+			style={{
+				width: 396,
+				boxShadow: 'rgba(100, 100, 111, 0.2) 0px 7px 29px 0px',
+			}}
+		>
+			<Grid item>
+				<Block click={() => {}}>
+					<Typography
+						variant='h4'
+						style={{ color: '#263b3a', fontWeight: 'bold' }}
+					>
+						MC
+					</Typography>
+				</Block>
+			</Grid>
+			<Grid item>
+				<Block click={() => {}}>
+					<Typography
+						variant='h4'
+						style={{ color: '#263b3a', fontWeight: 'bold' }}
+					>
+						M&#43;
+					</Typography>
+				</Block>
+			</Grid>
+			<Grid item>
+				<Block click={() => {}}>
+					<Typography
+						variant='h4'
+						style={{ color: '#263b3a', fontWeight: 'bold' }}
+					>
+						M&#8722;
+					</Typography>
+				</Block>
+			</Grid>
+
 			<Grid item>
 				<Block click={() => {}}>
 					<i
@@ -17,9 +54,15 @@ const Calculations = (props) => {
 					></i>
 				</Block>
 			</Grid>
+
 			<Grid item>
 				<Block click={() => {}}>
-					<i className='fas fa-plus fa-3x icon'></i>
+					<Typography
+						variant='h4'
+						style={{ color: '#263b3a', fontWeight: 'bold' }}
+					>
+						MR
+					</Typography>
 				</Block>
 			</Grid>
 
@@ -27,7 +70,7 @@ const Calculations = (props) => {
 				<Block click={() => {}}>
 					<Typography
 						variant='h4'
-						style={{ color: '#263b3a', fontWeight: 'bold' }}
+						style={{ color: '#9a0036', fontWeight: 'bold' }}
 					>
 						C
 					</Typography>
@@ -41,6 +84,151 @@ const Calculations = (props) => {
 			<Grid item>
 				<Button variant='contained' className='btn' color='primary'>
 					<i className='fas fa-divide fa-3x' style={{ color: '#ebf0f1' }}></i>
+				</Button>
+			</Grid>
+			<Grid item>
+				<Block click={() => {}}>
+					<Typography
+						variant='h4'
+						style={{ color: '#263b3a', fontWeight: 'bold' }}
+					>
+						7
+					</Typography>
+				</Block>
+			</Grid>
+			<Grid item>
+				<Block click={() => {}}>
+					<Typography
+						variant='h4'
+						style={{ color: '#263b3a', fontWeight: 'bold' }}
+					>
+						8
+					</Typography>
+				</Block>
+			</Grid>
+			<Grid item>
+				<Block click={() => {}}>
+					<Typography
+						variant='h4'
+						style={{ color: '#263b3a', fontWeight: 'bold' }}
+					>
+						9
+					</Typography>
+				</Block>
+			</Grid>
+
+			<Grid item>
+				<Button variant='contained' className='btn' color='primary'>
+					<i className='fas fa-asterisk fa-3x' style={{ color: '#ebf0f1' }}></i>
+				</Button>
+			</Grid>
+
+			<Grid item>
+				<Block click={() => {}}>
+					<Typography
+						variant='h4'
+						style={{ color: '#263b3a', fontWeight: 'bold' }}
+					>
+						4
+					</Typography>
+				</Block>
+			</Grid>
+			<Grid item>
+				<Block click={() => {}}>
+					<Typography
+						variant='h4'
+						style={{ color: '#263b3a', fontWeight: 'bold' }}
+					>
+						5
+					</Typography>
+				</Block>
+			</Grid>
+			<Grid item>
+				<Block click={() => {}}>
+					<Typography
+						variant='h4'
+						style={{ color: '#263b3a', fontWeight: 'bold' }}
+					>
+						6
+					</Typography>
+				</Block>
+			</Grid>
+			<Grid item>
+				<Button variant='contained' className='btn' color='primary'>
+					<i className='fas fa-minus fa-3x' style={{ color: '#ebf0f1' }}></i>
+				</Button>
+			</Grid>
+
+			<Grid item>
+				<Block click={() => {}}>
+					<Typography
+						variant='h4'
+						style={{ color: '#263b3a', fontWeight: 'bold' }}
+					>
+						1
+					</Typography>
+				</Block>
+			</Grid>
+
+			<Grid item>
+				<Block click={() => {}}>
+					<Typography
+						variant='h4'
+						style={{ color: '#263b3a', fontWeight: 'bold' }}
+					>
+						2
+					</Typography>
+				</Block>
+			</Grid>
+			<Grid item>
+				<Block click={() => {}}>
+					<Typography
+						variant='h4'
+						style={{ color: '#263b3a', fontWeight: 'bold' }}
+					>
+						3
+					</Typography>
+				</Block>
+			</Grid>
+			<Grid item>
+				<Button variant='contained' className='btn' color='primary'>
+					<i className='fas fa-plus fa-3x' style={{ color: '#ebf0f1' }}></i>
+				</Button>
+			</Grid>
+			<Grid item>
+				<Block click={() => {}}>
+					<Typography
+						variant='h4'
+						style={{ color: '#263b3a', fontWeight: 'bold' }}
+					>
+						&#43;/&#8722;
+					</Typography>
+				</Block>
+			</Grid>
+			<Grid item>
+				<Block click={() => {}}>
+					<Typography
+						variant='h4'
+						style={{ color: '#263b3a', fontWeight: 'bold' }}
+					>
+						0
+					</Typography>
+				</Block>
+			</Grid>
+			<Grid item>
+				<Block click={() => {}}>
+					<Typography
+						variant='h4'
+						style={{ color: '#263b3a', fontWeight: 'bold' }}
+					>
+						,
+					</Typography>
+				</Block>
+			</Grid>
+
+			<Grid item>
+				<Button variant='contained' className='btn' color='primary'>
+					<i className='fas fa-equals fa-3x' style={{ color: '#ebf0f1' }}></i>
 				</Button>
 			</Grid>
 		</Grid>
--- src/components/Screen/Screen.js
+++ src/components/Screen/Screen.js
@@ -2,23 +2,25 @@ import React from 'react';
 
 import PropTypes from 'prop-types';
 
-import { Container, Typography } from '@material-ui/core';
+import { Container, Typography, Grid } from '@material-ui/core';
 
 import classes from './Screen.module.css';
 
 const Screen = (props) => {
 	return (
-		<Container className={classes.Screen} maxWidth='xs'>
-			<Typography variant='h5' style={{ color: '#45565D' }} align='right'>
-				87
-			</Typography>
-			<Typography variant='h4' style={{ color: '#9CBEC8' }} align='right'>
-				{props.previous}
-			</Typography>
-			<Typography variant='h3' className={classes.current} align='right'>
-				{props.current}
-			</Typography>
-		</Container>
+		<Grid container style={{ width: 396 }} className={classes.Screen}>
+			<Container>
+				<Typography variant='h5' style={{ color: '#45565D' }} align='right'>
+					87
+				</Typography>
+				<Typography variant='h4' style={{ color: '#9CBEC8' }} align='right'>
+					{props.previous}
+				</Typography>
+				<Typography variant='h3' className={classes.current} align='right'>
+					{props.current}
+				</Typography>
+			</Container>
+		</Grid>
 	);
 };
 
